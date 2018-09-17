09e9603f5bef8729f0580ee9acd005e0e64e8d8a
ahmadmadar <35266736+ahmadmadar@users.noreply.github.com>
2018-09-17 15:30:42 -0600

    Created ChinookContext and AlbumController

---
+++ ChinookPractice/ChinookBackend/DAL/ChinookContext.cs
@@ -0,0 +1,31 @@
+﻿using ChinookBackend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookBackend.DAL
+{
+    internal class ChinookContext : DbContext
+    {
+        public ChinookContext() : base("")
+        {
+            Database.SetInitializer<ChinookContext>(null);
+        }
+
+        // Properties
+        public DbSet<Album> Albums {get; set;}
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceLine> InvoiceLines { get; set; }
+        public DbSet<MediaType> MediaTypes { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<PlaylistTrack> PlaylistTracks { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+    }
+}
